c24dabed5e2b6815faa9dd77f66c431ab9434881
ArielGolanski <ariariel12@gmail.com>
2024-05-28 20:47:14 +0300

    28/05/2024 20:47

--- Hemitube/web/src/App.js
+++ Hemitube/web/src/App.js
@@ -1,9 +1,33 @@
 import './App.css';
+import VideoListResults from './videoListResults/VideoListResults';
+import LeftMenu from './leftMenu/LeftMenu';
+import videos from './videoItem/videos';
+import Search from './search/Search';
+import { useState } from 'react';
+
+
 
 function App() {
+
+  const [videoList, setVideoList] = useState(videos);
+
+  const doSearch = function(q){
+    setVideoList(videos.filter((video)=> video.title.includes(q)));
+  }
+
+
+
   return (
-    <div className="App">
-      
+    <div className="container-fluid">
+      <div className="row">
+        <LeftMenu />
+        <div className="col main-content">
+          <Search doSearch={doSearch} />
+          <div className="row"></div>
+          <div className="row"></div>
+          <VideoListResults videos={videoList}/>
+        </div>
+      </div>
     </div>
   );
 }
